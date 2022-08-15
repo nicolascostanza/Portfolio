9321e0b19407b4e7b63b95ad76bfad44bceb0290
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-08-15 17:31:42 -0300

    dddd

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,7 +10,7 @@ import projImg7 from "../assets/img/automation3.png";
 import projImg8 from "../assets/img/manual1.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
-import TrackVisibility from "react-on-screen";
+import './projects.css'
 
 export const Projects = () => {
   const projects = [
@@ -34,58 +34,47 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility>
-              {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__fadeIn" : ""
-                  }
+            <div
+            >
+              <h2>Projects</h2>
+              <p>
+                In this section you will find the different projects made by me.
+                <li>
+                  <b>TRACKGENIX </b> is an App created for companies that are
+                  looking to hierarchize their company, manage their projects
+                  and have a control of the hours worked by each employee.
+                </li>
+              </p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Nav
+                  variant="pills"
+                  className="nav-pills mb-5 justify-content-center align-items-center classimportan"
+                  id="pills-tab"
                 >
-                  <h2>Projects</h2>
-                  <p>
-                    In this section you will find the different projects made by
-                    me.
-                    <li>
-                      <b>TRACKGENIX </b> is an App created for companies that are looking to hierarchize their company, manage their projects and have a control of the hours worked by each employee.
-                    </li>
-                  </p>
-                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                    {/* <Nav
-                      variant="pills"
-                      className="nav-pills mb-5 justify-content-center align-items-center"
-                      id="pills-tab"
-                    >
-                      <Nav.Item>
-                        <Nav.Link eventKey="first">Manual Testing</Nav.Link>
-                      </Nav.Item>
-                      <Nav.Item>
-                        <Nav.Link eventKey="second">
-                          Automation Testing
-                        </Nav.Link>
-                      </Nav.Item>
-                    </Nav> */}
-                    <Tab.Content
-                      id="slideInUp"
-                      className={
-                        isVisible ? "animate__animated animate__slideInUp" : ""
-                      }
-                    >
-                      <Tab.Pane eventKey="first">
-                        <Row>
-                          {projects.map((project, index) => {
-                            return <ProjectCard key={index} {...project} />;
-                          })}
-                        </Row>
-                      </Tab.Pane>
-                    </Tab.Content>
-                  </Tab.Container>
-                </div>
-              )}
-            </TrackVisibility>
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Deploy</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Repository</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+                <Tab.Content
+                  id="slideInUp"
+                >
+                  <Tab.Pane eventKey="first">
+                    <Row>
+                      {projects.map((project, index) => {
+                        return <ProjectCard key={index} {...project} />;
+                      })}
+                    </Row>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Tab.Container>
+            </div>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2} alt='IMG'></img>
+      <img className="background-image-right" src={colorSharp2} alt="IMG"></img>
     </section>
   );
 };
